6c4b11ed978e50b3c444d6f42daeacbcb5b8fb31
nikhilgupta30 <nikhilgupta311@gmail.com>
2018-03-31 14:45:37 +0530

    Initial commit

---
+++ android/src/com/zigzag/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.zigzag.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.zigzag.game.zigzag;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new zigzag(), config);
+	}
+}
